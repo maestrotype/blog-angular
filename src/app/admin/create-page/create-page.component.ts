b4c974de04038bc01f7e19305618988b8651a711
Andrey Danichkin <adan3891@gmail.com>
2021-03-17 23:06:10 +0200

    create post on server

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Post } from 'src/app/shared/interfaces';
+import { PostsService } from 'src/app/shared/posts.service';
 
 @Component({
   selector: 'app-create-page',
@@ -11,7 +12,7 @@ export class CreatePageComponent implements OnInit {
 
   form: FormGroup
 
-  constructor() { }
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -34,7 +35,11 @@ export class CreatePageComponent implements OnInit {
       date: new Date()
     }
 
-    console.log("post", post)
+    this.postsService.create(post).subscribe(() => {
+      this.form.reset()
+      
+    })
+    
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import { Environment } from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey:'AIzaSyDlOdL5ocncUQfIAt6BFcN6egRx80TDlTE'
+  apiKey:'AIzaSyDlOdL5ocncUQfIAt6BFcN6egRx80TDlTE',
+  fbDbUrl: 'https://blog-angular-aae15-default-rtdb.europe-west1.firebasedatabase.app'
 };
