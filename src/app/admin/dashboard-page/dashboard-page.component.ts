3983601e936bedc81a32f96e62352b01001a71e8
Andrey Danichkin <adan3891@gmail.com>
2021-03-19 12:47:48 +0200

    add search pipe

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PostsService } from 'src/app/shared/posts.service';
+import { Post } from 'src/app/shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = []
+  pSub: Subscription
+  searchStr=''
 
-  ngOnInit(): void {
+  constructor(private postsService: PostsService) { }
+
+  ngOnInit() {
+    this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  remove(id: string) {
+
+  }
+
+  ngOnDestroy() {
+    if(this.pSub) {
+      this.pSub.unsubscribe()
+    }
   }
 
 }
