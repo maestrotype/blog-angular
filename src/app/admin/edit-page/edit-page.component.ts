b0bb191160a56fd0e36fbed2450481b2ca07c8cf
Andrey Danichkin <adan3891@gmail.com>
2021-03-20 12:50:57 +0200

    start edit post

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { PostsService } from 'src/app/shared/posts.service';
+import { switchMap } from 'rxjs/operators';
+import { Post } from 'src/app/shared/interfaces';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
-  styleUrls: ['./edit-page.component.scss']
+  styleUrls: ['./edit-page.component.scss'],
 })
 export class EditPageComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          return this.postsService.getById(params['id']);
+        })
+      )
+      .subscribe((post: Post) => {
+        this.form = new FormGroup({
+          title: new FormControl(post.title, Validators.required),
+          text: new FormControl(post.text, Validators.required),
+        });
+      });
   }
-
 }
